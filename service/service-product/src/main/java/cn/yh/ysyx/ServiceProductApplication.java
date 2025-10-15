72cf5cbf8b006c1242a917a5facf8e96deeb78ba
handi <3283606583@qq.com>
2025-10-15 11:55:55 +0800

    feat(service): Nacos 服务注册发现

--- service/service-product/src/main/java/cn/yh/ysyx/ServiceProductApplication.java
+++ service/service-product/src/main/java/cn/yh/ysyx/ServiceProductApplication.java
@@ -2,8 +2,10 @@ package cn.yh.ysyx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Nacos 服务注册发现（可省略）
 public class ServiceProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceProductApplication.class, args);
