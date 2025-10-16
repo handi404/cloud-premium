6cc24f41ad07b19e5fc3338da06b4b4eeab1ce02
handi <3283606583@qq.com>
2025-10-16 09:46:28 +0800

    feat(service-product-client): ProductFeignClient

---
+++ service-client/service-product-client/src/main/java/cn/yh/ysyx/product/ProductFeignClient.java
@@ -0,0 +1,17 @@
+package cn.yh.ysyx.product;
+
+import cn.yh.ysyx.model.product.Category;
+import cn.yh.ysyx.model.product.SkuInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "service-product")
+public interface ProductFeignClient {
+
+    @GetMapping("/api/product/inner/getCategory/{categoryId}")
+    public Category getCategory(@PathVariable("categoryId") Long categoryId);
+
+    @GetMapping("/api/product/inner/getSkuInfo/{skuId}")
+    public SkuInfo getSkuInfo(@PathVariable("skuId") Long skuId);
+}
