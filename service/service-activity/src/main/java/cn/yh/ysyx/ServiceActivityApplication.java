7e092747c79ec32a13b0f217371f7ccd0643d43d
handi <3283606583@qq.com>
2025-10-17 09:15:16 +0800

    feat(service-activity): init project setup

---
+++ service/service-activity/src/main/java/cn/yh/ysyx/ServiceActivityApplication.java
@@ -0,0 +1,11 @@
+package cn.yh.ysyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceActivityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceActivityApplication.class, args);
+    }
+}
